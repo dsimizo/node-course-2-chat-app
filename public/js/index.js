3bf45b06d5dd7a921dcf75ddc8b9b4987f897b0d
dsimizo <dsimizo@gmail.com>
2018-03-20 18:16:25 -0300

    Scroll to bottom if user is close to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,21 @@
 var socket = io();
 
+function scrollToBotom() {
+  // Selectors
+  var messages = jQuery('#messages');
+  var newMessage = messages.children('li:last-child');
+  // Heights
+  var clientHeight = messages.prop('clientHeight');
+  var scrollTop = messages.prop('scrollTop');
+  var scrollHeight = messages.prop('scrollHeight');
+  var newMessageHeight = newMessage.innerHeight();
+  var lastMessageHeight = newMessage.prev().innerHeight();
+
+  if(clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+    messages.scrollTop(scrollHeight); 
+  }
+}
+
 socket.on('connect', function() {
   console.log('Connected to the server');
 });
@@ -18,6 +34,7 @@ socket.on('newMessage', function(message) {
   });
 
   jQuery('#messages').append(html);
+  scrollToBotom();
 });
 
 socket.on('newLocationMessage', function(message) {
@@ -30,6 +47,7 @@ socket.on('newLocationMessage', function(message) {
   });
 
   jQuery('#messages').append(html);
+  scrollToBotom();
 });
 
 // socket.emit('createMessage', {
